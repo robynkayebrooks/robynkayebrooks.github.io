a159d8e3308fed2ea9b181dbc50bbf2404b0e9d3
robynkayebrooks <robynkaye4@gmail.com>
2017-08-22 10:14:23 -0500

    added scroll to top button

---
+++ js/_main.js
@@ -0,0 +1,7 @@
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
